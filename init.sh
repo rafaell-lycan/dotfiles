9c7e6e212b0a649fb71a6e037c21d6c43750ed5b
Rafael Almeida <rafael.almeida@tripda.com.br>
2015-01-28 12:55:48 -0200

    Update setup_osx to configure PHP with Homebrew

--- init.sh
+++ init.sh
@@ -2,9 +2,9 @@
 source $HOME/dotfiles/setup_osx.sh
 
 #Configure other stuffs
-source $HOME/dotfiles/scripts/alias.sh
-source $HOME/dotfiles/scripts/export.sh
-source $HOME/dotfiles/scripts/other.sh
-source $HOME/dotfiles/scripts/git-completion.sh
-source $HOME/dotfiles/scripts/prompt.sh
-source $HOME/dotfiles/scripts/awsenv.sh
+#source $HOME/dotfiles/scripts/alias.sh
+#source $HOME/dotfiles/scripts/export.sh
+#source $HOME/dotfiles/scripts/other.sh
+#source $HOME/dotfiles/scripts/git-completion.sh
+#source $HOME/dotfiles/scripts/prompt.sh
+#source $HOME/dotfiles/scripts/awsenv.sh
--- setup_osx.sh
+++ setup_osx.sh
@@ -9,13 +9,22 @@ curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | 
 echo "Installing homebrew"
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+echo "Installing Xcode command line tools"
+xcode-select --install
+
 echo "Installing brew dependencies"
 brew install tmux
 brew install htop-osx
 brew install openssl
+brew install icu4c
 brew install wget
 brew install git
-brew install nvm
+brew install git-extras
 
 echo "Installing last version of NVM"
+brew install nvm
 nvm install stable
+
+echo "Installing PHP Env"
+brew install php56 --without-apache --with-fpm --with-homebrew-openssl --with-mssql --with-postgresql
+brew install phpunit
