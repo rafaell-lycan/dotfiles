ea6c916b30ab4f1d43271180eb547c0fa1ba399c
Rafaell Lycan <sonny.webdsg@gmail.com>
2016-04-28 15:03:00 -0300

    Fix setup with log (copy)

--- setup.sh
+++ setup.sh
@@ -1,12 +1,38 @@
 #!/bin/sh
 
-#
+# Tells the shell script to exit if it encounters an error
 set -e
 
-source log.sh
+# -- Log
+# Duplicated code from log.sh
+# since we cannot import a file when installing via cURL
 
-# -- Homebrew
-# ----------------------------------------------------------------------
+function msg {
+  echo  "\033[0;37m$1\033[0m";
+}
+
+function msg_ok {
+  echo  "➜\033[1;32m $1 ✔\033[0m";
+}
+
+function msg_run {
+  echo  "➜\033[1;35m $1 $ $2\033[0m";
+}
+
+function msg_done {
+  echo  "➜\033[1;37m $1 \033[0m";
+}
+
+# -- Hello :) ------------------------------------------------------------------
+msg '									                    ' 
+msg '     | |     | |  / _(_) |           '
+msg '   __| | ___ | |_| |_ _| | ___  ___  '
+msg '  / _` |/ _ \| __|  _| | |/ _ \/ __| '
+msg ' | (_| | (_) | |_| | | | |  __/\__ \ '
+msg '  \__,_|\___/ \__|_| |_|_|\___||___/ '
+msg '									                    ' 
+
+# -- Homebrew ------------------------------------------------------------------
 if which brew &>/dev/null; then
   msg_ok "homebrew"
 else
@@ -22,8 +48,7 @@ else
   brew install git
 fi
 
-# -- Dotfiles
-# ----------------------------------------------------------------------
+# -- Dotfiles ------------------------------------------------------------------
 if [[ -d "$HOME/.dotfiles" ]]; then
   msg_ok "dotfiles"
 else
@@ -31,9 +56,10 @@ else
   git clone https://github.com/rafaell-lycan/dotfiles.git $HOME/.dotfiles
 fi
 
-# -- Installers
-  cd "$HOME/.dotfiles/$(dirname $)"/..
-  find . -name install.sh | while read installer ; do sh -c "${installer}" ; done
+# -- Installers ------------------------------------------------------------------
+# Find the installers and run them iteratively
+cd "$HOME/.dotfiles/$(dirname $)"/..
+find . -name install.sh | while read installer ; do sh -c "${installer}" ; done
 
 #echo "Set defaults evn"
 #defaults write -g ApplePressAndHoldEnabled -bool false;
